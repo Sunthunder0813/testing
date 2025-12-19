e5ba37e88232117615feb2f2a968cc2ea6d6fd15
Joseph Cañete Santander <santanderjoseph13@gmail.com>
2025-12-19 16:26:14 +0800

    Update camera URLs for RTSP streams

--- sample.py
+++ sample.py
@@ -6,8 +6,8 @@ from hailo_platform import HEF, VDevice, InferVStreams
 
 # ---------------- CONFIG ----------------
 CAMERA_URLS = [
-    "rtsp://user:password@192.168.18.2:554/stream1",
-    "rtsp://user:password@192.168.18.71:554/stream1"
+    "rtsp://admin@192.168.18.2:554/stream1",
+    "rtsp://admin@192.168.18.71:554/stream1"
 ]
 HEF_PATH = "yolov8n_person.hef"
 INPUT_SIZE = 640
